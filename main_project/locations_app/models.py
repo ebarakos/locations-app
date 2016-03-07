4138c8e3e96ee3214f52e01d7f905ae27669d559
mparakose <e.mparakos@gmail.com>
2016-03-08 00:13:07 +0200

    save location to both database and fusion table, after reverse geocoding

--- main_project/locations_app/models.py
+++ main_project/locations_app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class Location(models.Model):
-	lat = models.FloatField()
-	lng = models.FloatField()
+	lat = models.CharField(max_length=10)
+	lng = models.CharField(max_length=10)
 	address = models.CharField(max_length=100)
--- main_project/locations_app/views.py
+++ main_project/locations_app/views.py
@@ -1,23 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
 from .models import Location
 from httplib2 import Http
 from oauth2client.service_account import ServiceAccountCredentials
 import os
 import requests
+import json
 from apiclient.discovery import build
 
 
 def index(request):
  	return render(request, "index.html")
 
-@csrf_exempt
 def create_post(request):
 	if request.method == 'POST':
 		lat = request.POST.get('lat')
 		lng = request.POST.get('lng')
-		loc = Location(address=lat,lat=lat,lng=lng)
+		address = request.POST.get('address')
+		loc = Location(address=address,lat=lat,lng=lng)
 		loc.save()
 		insertRow(loc)
 		return HttpResponse(
@@ -32,7 +32,11 @@ def create_post(request):
 
 def insertRow(loc):
   table_id = "1oGMujUCtTgLLx6LjzAvCfGPSd1CtFegQvfY3-dmP"
-  query = "INSERT INTO {}(Address,Lat,Lng) VALUES({},{},{})".format(table_id,loc.address,loc.lat,loc.lng) # Single quotes
+  print(loc.address)
+
+  query = "INSERT INTO {}(Address,Lat,Lng) VALUES('{}',{},{})".format(table_id,loc.address,loc.lat,loc.lng) # Single quotes
+  print(loc.lat)
+  print(loc.lng)
   scopes = ['https://www.googleapis.com/auth/fusiontables']
   HOME = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
   credentials = ServiceAccountCredentials.from_json_keyfile_name(os.path.join(HOME, 'test.json'), scopes=scopes)
